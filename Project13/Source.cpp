a904dee067aa2ac29ff09bc7d350bf537485410e
Kris Crona <brandon.heggs@yahoo.com>
2022-12-04 21:20:00 -0500

    Add project files.

---
+++ Project13/Source.cpp
@@ -0,0 +1,31 @@
+// Brandon Heggs
+//CIS 1202 501
+//November 26, 2022
+
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+char character(char start, int offset)
+{
+	int startAsciiValue = int(start);
+	int target = startAsciiValue + offset;
+	try
+	{
+		if 
+		{
+			string invalidCharacterExcpeption = "ERROR: invalid charcter!";
+			return 0;
+		}
+	}
+
+	catch
+}
+
+
+
+main()
+{ 
+
+}
